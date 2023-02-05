24f95a0c37bb15fb177c8f6340dc5c0636b69ab7
buttura <higordonay@gmail.com>
2023-02-05 15:16:12 -0300

    first commit

---
+++ DataGridViewEx/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace DataGridViewEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVenda = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.Descricao = new System.Windows.Forms.Label();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.txtValorUnitario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DescricaoDGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuantidadeBox = new System.Windows.Forms.TextBox();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnCancelarVenda = new System.Windows.Forms.Button();
+            this.btnNovaVenda = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRowCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Venda:";
+            // 
+            // txtVenda
+            // 
+            this.txtVenda.Location = new System.Drawing.Point(38, 40);
+            this.txtVenda.Name = "txtVenda";
+            this.txtVenda.Size = new System.Drawing.Size(100, 20);
+            this.txtVenda.TabIndex = 1;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(38, 79);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(100, 20);
+            this.txtDescricao.TabIndex = 2;
+            // 
+            // Descricao
+            // 
+            this.Descricao.AutoSize = true;
+            this.Descricao.Location = new System.Drawing.Point(35, 63);
+            this.Descricao.Name = "Descricao";
+            this.Descricao.Size = new System.Drawing.Size(58, 13);
+            this.Descricao.TabIndex = 3;
+            this.Descricao.Text = "Descricao:";
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(35, 105);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(65, 13);
+            this.lblQuantidade.TabIndex = 4;
+            this.lblQuantidade.Text = "Quantidade:";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(38, 121);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantidade.TabIndex = 5;
+            // 
+            // txtValorUnitario
+            // 
+            this.txtValorUnitario.Location = new System.Drawing.Point(38, 163);
+            this.txtValorUnitario.Name = "txtValorUnitario";
+            this.txtValorUnitario.Size = new System.Drawing.Size(100, 20);
+            this.txtValorUnitario.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Valor Unitario:";
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(155, 35);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(231, 69);
+            this.btnInserir.TabIndex = 8;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(155, 111);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(231, 69);
+            this.btnRemover.TabIndex = 9;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DescricaoDGV,
+            this.Quant,
+            this.VUnitario});
+            this.dataGridView1.Location = new System.Drawing.Point(38, 200);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(348, 150);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // DescricaoDGV
+            // 
+            this.DescricaoDGV.HeaderText = "Descricao";
+            this.DescricaoDGV.Name = "DescricaoDGV";
+            this.DescricaoDGV.ReadOnly = true;
+            // 
+            // Quant
+            // 
+            this.Quant.HeaderText = "Quant";
+            this.Quant.Name = "Quant";
+            this.Quant.ReadOnly = true;
+            // 
+            // VUnitario
+            // 
+            this.VUnitario.HeaderText = "V. Unitario";
+            this.VUnitario.Name = "VUnitario";
+            this.VUnitario.ReadOnly = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Quantidade de Item Selecionado:";
+            // 
+            // txtQuantidadeBox
+            // 
+            this.txtQuantidadeBox.Location = new System.Drawing.Point(206, 359);
+            this.txtQuantidadeBox.Name = "txtQuantidadeBox";
+            this.txtQuantidadeBox.Size = new System.Drawing.Size(46, 20);
+            this.txtQuantidadeBox.TabIndex = 13;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(258, 356);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(128, 28);
+            this.btnAlterar.TabIndex = 14;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnCancelarVenda
+            // 
+            this.btnCancelarVenda.Location = new System.Drawing.Point(258, 385);
+            this.btnCancelarVenda.Name = "btnCancelarVenda";
+            this.btnCancelarVenda.Size = new System.Drawing.Size(128, 29);
+            this.btnCancelarVenda.TabIndex = 15;
+            this.btnCancelarVenda.Text = "Cancelar Venda";
+            this.btnCancelarVenda.UseVisualStyleBackColor = true;
+            this.btnCancelarVenda.Click += new System.EventHandler(this.btnCancelarVenda_Click);
+            // 
+            // btnNovaVenda
+            // 
+            this.btnNovaVenda.Location = new System.Drawing.Point(258, 416);
+            this.btnNovaVenda.Name = "btnNovaVenda";
+            this.btnNovaVenda.Size = new System.Drawing.Size(128, 27);
+            this.btnNovaVenda.TabIndex = 16;
+            this.btnNovaVenda.Text = "Nova Venda";
+            this.btnNovaVenda.UseVisualStyleBackColor = true;
+            this.btnNovaVenda.Click += new System.EventHandler(this.btnNovaVenda_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(258, 445);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(128, 23);
+            this.btnFechar.TabIndex = 17;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Total:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.AutoSize = true;
+            this.txtTotal.Location = new System.Drawing.Point(75, 396);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(45, 13);
+            this.txtTotal.TabIndex = 19;
+            this.txtTotal.Text = "R$ 0,00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 434);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Itens na grade:";
+            // 
+            // txtRowCount
+            // 
+            this.txtRowCount.AutoSize = true;
+            this.txtRowCount.Location = new System.Drawing.Point(119, 434);
+            this.txtRowCount.Name = "txtRowCount";
+            this.txtRowCount.Size = new System.Drawing.Size(13, 13);
+            this.txtRowCount.TabIndex = 21;
+            this.txtRowCount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 480);
+            this.Controls.Add(this.txtRowCount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnNovaVenda);
+            this.Controls.Add(this.btnCancelarVenda);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.txtQuantidadeBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtValorUnitario);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.lblQuantidade);
+            this.Controls.Add(this.Descricao);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtVenda);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVenda;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label Descricao;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtValorUnitario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescricaoDGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VUnitario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtQuantidadeBox;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnCancelarVenda;
+        private System.Windows.Forms.Button btnNovaVenda;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label txtTotal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label txtRowCount;
+    }
+}
+
---
+++ DataGridViewEx/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewEx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(txtDescricao.Text, txtQuantidade.Text, txtValorUnitario.Text);
+            txtVenda.Clear(); txtDescricao.Clear(); txtQuantidade.Clear(); txtValorUnitario.Clear();
+            txtRowCount.Text = dataGridView1.RowCount.ToString();
+            int total = 0;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                total += Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value) * Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value);
+            }
+            txtTotal.Text = total.ToString("C");
+            MessageBox.Show("Venda inserida com sucesso!", "Inserir", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.CurrentCell.RowIndex);
+            }
+            txtRowCount.Text = dataGridView1.RowCount.ToString();
+            int total = 0;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                total += Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value) * Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value);
+            }
+            txtTotal.Text = total.ToString("C");
+            MessageBox.Show("Venda removida com sucesso!", "Remover", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentRow.Cells[1].Value = txtQuantidade.Text;
+            dataGridView1.CurrentRow.Cells[1].Value = txtQuantidadeBox.Text;
+            int total = 0;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                total += Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value) * Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value);
+            }
+            txtTotal.Text = total.ToString("C");
+            MessageBox.Show("Quantidade alterada com sucesso!", "Alteracao", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.RowCount > 0)
+            {
+                txtQuantidade.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtQuantidadeBox.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            }
+        }
+
+        private void btnNovaVenda_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            MessageBox.Show("Pronto para a nova venda!", "Nova Venda", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            txtVenda.Clear(); txtDescricao.Clear(); txtQuantidade.Clear(); txtValorUnitario.Clear();
+        }
+
+        private void btnCancelarVenda_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            MessageBox.Show("Venda cancelada!", "Venda Cancelada", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            txtVenda.Clear(); txtDescricao.Clear(); txtQuantidade.Clear(); txtValorUnitario.Clear();
+        }
+    }
+}
